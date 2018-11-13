d6176085c5251bf1673ce2109af8bbdc051983d3
melissachamilton <melissac.hamilton1@gmail.com>
2018-11-13 14:12:08 -0500

    commenting out to fix heroku

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,18 +1,16 @@
 class Billing < ApplicationRecord
-  include HTTParty
-  base_uri 'https://api.stripe.com'
+  # include HTTParty
+  # base_uri 'https://api.stripe.com'
 
-  def self.generate(api_id)
-    payment = find_by api_id: api_id
-    return payment unless payment.nil?
+  # def self.generate(api_id)
+  #   payment = find_by api_id: api_id
+  #   return payment unless payment.nil?
 
-    response = get "/#{api_id}"
-  placeholder_image = "https://www.placecage.com/c/#{200 + api_id}/#{200 + api_id}"
+  #   response = get "/#{api_id}"
+  # placeholder_image = "https://www.placecage.com/c/#{200 + api_id}/#{200 + api_id}"
 
-  create!(name: response['name'],
-    image: placeholder_image,
-    api_id: api_id)
-      
-    
-  end
+  # create!(name: response['name'],
+    # image: placeholder_image,
+  #   api_id: api_id)
+  # end
 end
